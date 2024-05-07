251f8c85661ed3ad92183b9efe9c48550002ecc9
Pasha Bitz <pasha@convex.dev>
2024-05-07 10:30:53 -0700

    Setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://tidy-elf-32.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/ConvexClientProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { ReactNode } from "react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+    return <ClerkProvider publishableKey="pk_test_dGlkeS1lbGYtMzIuY2xlcmsuYWNjb3VudHMuZGV2JA">
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  </ClerkProvider>
+}
