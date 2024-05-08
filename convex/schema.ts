ee0fc2f3153a333c4ff479b8fca07147b124634a
Pasha Bitz <pasha@convex.dev>
2024-05-07 19:33:57 -0700

    Add schema and index on forms.slug

---
+++ convex/schema.ts
@@ -0,0 +1,25 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    forms: defineTable({
+        createdBy: v.string(),
+        description: v.optional(v.string()),
+        name: v.optional(v.string()),
+        slug: v.string(),
+      }).index("by_slug", ["slug"]),
+      form_responses: defineTable({
+        formId: v.id("forms"),
+        slug: v.optional(v.string()),
+        values: v.array(
+          v.object({ name: v.string(), value: v.string() })
+        ),
+      }),
+      form_fields: defineTable({
+        formId: v.string(),
+        name: v.string(),
+        order: v.float64(),
+        selectOptions: v.optional(v.array(v.string())),
+        type: v.string(),
+      }),
+});
