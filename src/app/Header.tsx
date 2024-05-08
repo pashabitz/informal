5e8982eecd72febf6c268192029653655ee86064
Pasha Bitz <pasha@convex.dev>
2024-05-07 19:56:16 -0700

    Rainbow

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,12 +1,18 @@
 "use client";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+import { useState } from "react";
 
 export default function Header() {
-  
+  const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+  const word = 'Informal';
+  const rainbowWord = word.split('').map((letter, i) => (
+    <span style={{ color: rainbowColors[i % rainbowColors.length] }}>{letter}</span>
+  ));
+
   return (
-      <header className="border-b-2 border-blue flex justify-between items-center mb-2">
-        <h1 className="ml-2"><a href="/">Informal</a></h1>
+      <header className="border-b-2 border-blue flex justify-between items-center my-2">
+        <h1 className="ml-2" ><a href="/" >{rainbowWord}</a></h1>
         <div className="inline-block mr-2">
           <Unauthenticated>
             <SignInButton/>
