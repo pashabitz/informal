4a411940858d335d391f4ae55664ebd9a35d1132
Pasha Bitz <pasha@convex.dev>
2024-05-10 14:52:41 -0700

    init shadcn

--- src/app/UserForms.tsx
+++ src/app/UserForms.tsx
@@ -2,6 +2,8 @@ import React, { use } from 'react';
 import { Authenticated, Unauthenticated, useQuery, useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { Id } from '../../convex/_generated/dataModel';
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
 
 export default function UserForms() {
   const createForm = useMutation(api.forms.create);
@@ -25,23 +27,23 @@ export default function UserForms() {
    {forms && forms.length > 0 ? (
     <>
     <h2 className="mt-0">Your forms:</h2>
-    <table>
-        <tbody>
+    <Table>
+        <TableBody>
         {forms.map((form) => (
-            <tr key={form._id}>
-                <td><a href={`/edit-form/${form._id}`}>{form._id}</a></td>
-                <td>{form.name}</td>
-                <td>{form.description}</td>
-                <td><span onClick={(e) => handleDelete(form._id)} className="delete-button"></span></td>
-            </tr>
+            <TableRow key={form._id}>
+                <TableCell><a href={`/edit-form/${form._id}`}>{form._id}</a></TableCell>
+                <TableCell>{form.name}</TableCell>
+                <TableCell>{form.description}</TableCell>
+                <TableCell><span onClick={(e) => handleDelete(form._id)} className="delete-button"></span></TableCell>
+            </TableRow>
         ))}
-        </tbody>
-    </table>
+        </TableBody>
+    </Table>
     </>
    ) : (
         <p>You don&apos;t have any forms yet. </p>
 
    )}
-   <button onClick={handleCreateClick}>New</button>
+   <Button onClick={handleCreateClick}>New</Button>
     </>
 }
