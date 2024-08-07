01ce6ef393429a79f862ac06ee0116911b9dff9a
Pasha Bitz <pasha@convex.dev>
2024-08-07 16:17:21 -0700

    Add http action

---
+++ convex/http.ts
@@ -0,0 +1,13 @@
+import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
+
+const http = httpRouter();
+
+http.route({
+    path: "/form",
+    method: "GET", 
+    handler: httpAction(async (ctx, request) => {
+        return new Response("Form", { status: 200 });
+    }),
+});
+export default http;
